60e2b0b525e8c92dc2688bb638e6fb19df577fbf
bottenkamp <B7fg*kli#>
2018-07-20 12:15:02 +0200

    first commit

---
+++ jhipster-app/src/main/webapp/app/shared/shared.module.ts
@@ -0,0 +1,51 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+import {
+    Baeldungapp2SharedLibsModule,
+    Baeldungapp2SharedCommonModule,
+    CSRFService,
+    AuthServerProvider,
+    AccountService,
+    UserService,
+    StateStorageService,
+    LoginService,
+    LoginModalService,
+    JhiLoginModalComponent,
+    Principal,
+    JhiTrackerService,
+    HasAnyAuthorityDirective,
+} from './';
+
+@NgModule({
+    imports: [
+        Baeldungapp2SharedLibsModule,
+        Baeldungapp2SharedCommonModule
+    ],
+    declarations: [
+        JhiLoginModalComponent,
+        HasAnyAuthorityDirective
+    ],
+    providers: [
+        LoginService,
+        LoginModalService,
+        AccountService,
+        StateStorageService,
+        Principal,
+        CSRFService,
+        JhiTrackerService,
+        AuthServerProvider,
+        UserService,
+        DatePipe
+    ],
+    entryComponents: [JhiLoginModalComponent],
+    exports: [
+        Baeldungapp2SharedCommonModule,
+        JhiLoginModalComponent,
+        HasAnyAuthorityDirective,
+        DatePipe
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+
+})
+export class Baeldungapp2SharedModule {}
